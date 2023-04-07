29d533f27741159a30eff7ee2da42ceaae7fbed6
ahmedmaher179200 <73036767+ahmedmaher179200@users.noreply.github.com>
2023-04-07 23:54:07 +0200

    Add files via upload

---
+++ src/CrudServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ahmedmaher\crud;
+
+use Illuminate\Support\ServiceProvider;
+
+class CrudServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([__DIR__ . '/Commands' => base_path('Console/Commands')]);
+    }
+}
