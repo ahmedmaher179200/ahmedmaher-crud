1329fbb79654e81e401ef850c298cca899d4bf30
ahmedmaher179200 <73036767+ahmedmaher179200@users.noreply.github.com>
2023-04-15 17:03:42 +0200

    Add files via upload

--- src/CrudServiceProvider.php
+++ src/CrudServiceProvider.php
@@ -23,9 +23,8 @@ class CrudServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
         $this->publishes([__DIR__ . '/Commands' => base_path('app/Console/Commands')]);
-        $this->publishes([__DIR__ . '/Models' => base_path('app/CrudGenerate')]);
         $this->publishes([__DIR__ . '/Views' => base_path('app/CrudGenerate/CrudViews')]);
+        $this->publishes([__DIR__ . '/Classes' => base_path('app/CrudGenerate')]);
     }
 }
