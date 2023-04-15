9f66c97cabab7ec7ee077233f3520f5dfce7e3a9
ahmedmaher179200 <73036767+ahmedmaher179200@users.noreply.github.com>
2023-04-15 17:56:02 +0200

    Add files via upload

--- src/builder/helperBuilder.php
+++ src/builder/helperBuilder.php
@@ -13,7 +13,7 @@ class helperBuilder
 
     public static function makeModel($table, $model){
         $newfile = fopen(base_path('app/Models/' . $model . '.php'),'w');       //create model
-        $file_content = file_get_contents(base_path('app/CrudGenerate/model.php'));  //take content
+        $file_content = file_get_contents(base_path('app/CrudGenerate/model.txt'));  //take content
         $content = str_replace('@@table@@', $table ,$file_content);      //handel content
         $content = str_replace('@@model@@', $model ,$content);      //handel content
         fwrite($newfile, $content);
@@ -29,7 +29,7 @@ class helperBuilder
 
     public static function makeViews($table, $path){
         File::makeDirectory(base_path('resources/views/'. $path . '/' . $table), 0777, true, true);
-        
+
         $indexViewPath = base_path('resources/views/' . $path  . '/' . $table . '/index.blade.php');
         $createViewPath = base_path('resources/views/' . $path  . '/' . $table . '/create.blade.php');
         $editViewPath = base_path('resources/views/' . $path  . '/' . $table . '/edit.blade.php');;
@@ -45,7 +45,7 @@ class helperBuilder
         File::makeDirectory(base_path('app/Http/Controllers/'. $path), 0777, true, true);
 
         $newfile = fopen(base_path('app/Http/Controllers/' . $path . '/'. $model .'Controller.php'),'w'); //create Controller
-        $file_content = file_get_contents(base_path('app/CrudGenerate/controller.php'));  //take content
+        $file_content = file_get_contents(base_path('app/CrudGenerate/controller.txt'));  //take content
         $content = str_replace('@@table@@', $table ,$file_content);      //handel content
         $content = str_replace('@@model@@', $model ,$content);      //handel content
         fwrite($newfile, $content);
